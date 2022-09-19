5ce52c91b1bda2e3a816a7d55c2321441cf833a7
Adam Hunter <90648351+adam-hunter13@users.noreply.github.com>
2022-09-18 20:46:11 -0600

    edit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log("Hello Modere Team! I hope you enjoy my product!")
+console.log("Thanks for taking the time to interview me!")
 
 // Invite form Event Listener
 let submitForm = document.getElementById('sbt')
